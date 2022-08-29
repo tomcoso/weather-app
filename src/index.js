e49f33d0d22f3d028b1019a5be65b1aa43ed4b2d
tomcoso <tomasdessy0@gmail.com>
2022-08-29 18:10:33 +0100

    style responsive search bar

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
 import './style.css'
 import getWeather from './weather'
 import owIcon from './assets/openweather-logo.png'
+import git from './assets/GitHub-Mark-Light-32px.png'
 import { add, format } from 'date-fns'
 import pickBg from './background'
+import searchIcon from './assets/search.svg'
 
 const dom = {
   page: document.querySelector('#page-wrap'),
-  searchInput: document.querySelector('#search-panel > input'),
-  searchBtn: document.querySelector('#search-panel > button'),
-  openweather: document.querySelector('footer > div img'),
+  searchInput: document.querySelector('#search-input > input'),
+  searchBtn: document.querySelector('#search-input > button'),
+  searchInputWrap: document.querySelector('#search-input'),
+  openweather: document.querySelector('footer > div > a > img'),
   locationName: document.querySelector('#location-name'),
   temp: document.querySelector('#temp'),
-  sky: document.querySelector('#sky'),
+  description: document.querySelector('#description'),
   humidity: document.querySelector('#humidity'),
   time: document.querySelector('#time'),
   wind: document.querySelector('#wind'),
   errormsg: document.querySelector('#error-msg'),
   visibility: document.querySelector('#visibility'),
-  feelsLike: document.querySelector('#feels-like'),
+  git: document.querySelector('footer > a > img'),
+  searchPop: document.querySelector('#search-pop > img'),
 }
 
-getWeather('london').then((res) => render(res))
+getWeather('buenos aires').then((res) => render(res))
 dom.openweather.src = owIcon
+dom.git.src = git
+dom.searchPop.src = searchIcon
 
 const search = function (e) {
   if (e.key === 'Enter' || e.type === 'click') {
@@ -37,7 +43,7 @@ const search = function (e) {
           dom.errormsg.animate(
             [
               { opacity: 0 },
-              { opacity: 1, offset: 0.2 },
+              { opacity: 1, offset: 0.1 },
               { opacity: 1, offset: 1 },
             ],
             {
@@ -70,13 +76,38 @@ const render = function (data) {
   )
   dom.locationName.textContent = `${data.name}, ${data.sys.country}`
   dom.temp.textContent = `${data.main.temp} °C`
-  dom.sky.textContent =
+  dom.description.innerHTML = `${
     data.weather[0].description[0].toUpperCase() +
     data.weather[0].description.slice(1)
+  }. <br> Feels like ${data.main.feels_like} °C`
   dom.humidity.textContent = `${data.main.humidity}% humidity`
   dom.wind.textContent = `${(data.wind.speed * 3.6).toFixed(1)}km/h`
   dom.visibility.textContent = `${(data.visibility / 1000).toFixed(
     1
   )}km visibility`
-  dom.feelsLike.textContent = `Feels like ${data.main.feels_like} °C`
 }
+
+const searchAnimation = new Animation(
+  new KeyframeEffect(
+    dom.searchInputWrap,
+    [
+      { top: '0%', opacity: 0, offset: 0 },
+      { top: '-150%', opacity: 1, 'z-index': 2, offset: 1 },
+    ],
+    {
+      duration: 300,
+      direction: 'alternate',
+      iterations: 1,
+      easing: 'linear',
+      fill: 'forwards',
+    }
+  )
+)
+
+dom.searchPop.addEventListener('click', () => {
+  if (searchAnimation.playState === 'finished') {
+    searchAnimation.reverse()
+  } else {
+    searchAnimation.play()
+  }
+})
